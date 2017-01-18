c552ff6af9ab23cdac99a9a37190247df069527b
15kmj03 <15kmj03@ms.dendai.ac.jp>
2017-01-18 12:44:27 +0900

    センサデータ修正

--- AutoRun.m
+++ AutoRun.m
@@ -1,13 +1,7 @@
-for personid=1:8
+for personid=5:8
     for num=1:5
-        if personid==3&&num==3
-            continue
-        end
-        if personid==8&&num==3
-            continue
-        end
-        loadDirName='C:\Users\H87-PRO\Desktop\30deg\';
-        saveDirName='C:\Users\H87-PRO\Desktop\result\';
+        loadDirName='C:\Users\exp1\Documents\MATLAB\HeadPoseEstimation\data\';
+        saveDirName='C:\Users\exp1\Documents\MATLAB\HeadPoseEstimation\result\';
         personname=num2str(personid);
         numname=num2str(num);
         if personid==7
@@ -18,7 +12,7 @@ for personid=1:8
         
         
         loadFileName=[loadDirName,personname,'\',numname,suffix];
-        loadFileNameS=[loadDirName,personname,'\',numname,'_sens_betas.mat'];
+        loadFileNameS=[loadDirName,personname,'\',numname,'_sens_yaws.mat'];
         saveFileNameP=[saveDirName,personname,'\',numname,'_pitches'];
         saveFileNameY=[saveDirName,personname,'\',numname,'_yaws'];
         saveFileNameR=[saveDirName,personname,'\',numname,'_rolls'];
@@ -31,13 +25,16 @@ for personid=1:8
         x=1:300;
         yaws=yaws(1:300);
         figure(1)
-        plot(x,yaws,x,sens_betas)
+        plot(x,yaws,x,sens_betas,'--')
+        xlim([0,310])
         ylim([-30,30])
         grid on
-        xlabel('時間 [s]')
+        xlabel('時間 [sec]')
         ylabel('ヨー角度 [deg]')
-        
-        
+        legend('推定値','センサ値')
+        ax = gca;
+        ax.XAxisLocation = 'origin';
+        ax.XTickLabel = 0:2.5:15;
         
         save(saveFileNameP,'pitches')
         save(saveFileNameY,'yaws')
